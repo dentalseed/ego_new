9af981342d406fd3a499806fb7b1927b1dfc0026
den.b <den.b@DESKTOP-KME29H2>
2024-05-24 15:20:01 +0900

    first commit

---
+++ src/main/java/egovframework/example/EgovBootApplication.java
@@ -0,0 +1,14 @@
+package egovframework.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgovBootApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ddddddddd");
+		SpringApplication.run(EgovBootApplication.class, args);
+	}
+
+}
