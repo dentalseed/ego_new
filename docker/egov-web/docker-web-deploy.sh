ee9f632887d1542b62655dfd5e49fa04ebe1c4f1
dentalseed <155122919+dentalseed@users.noreply.github.com>
2024-05-27 18:34:31 +0900

    Update docker-web-deploy.sh
    test

--- docker/egov-web/docker-web-deploy.sh
+++ docker/egov-web/docker-web-deploy.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <server_address>"
     exit 1
 fi
 
-URL="$1:8080/health"
-SUCCESS_MESSAGE="SUCCESS"
-MAX_ATTEMPTS=40
-DELAY_SECONDS=1
 # 도커 스탑, 삭제
 docker stop $1
 docker rm $1
@@ -15,22 +10,6 @@ docker rm $1
 # api 배포 test
 docker compose -p $1 -f  ./dev/$1-compose.yml build && docker compose -p $1 -f  ./dev/$1-compose.yml up -d
 
-# 실행 확인1 1213
-attempt=1
-while [ $attempt -le $MAX_ATTEMPTS ]
-do
-    echo "Attempting health check for server $1, attempt: $attempt"
-    response=$(curl -sS "$URL")
-    if [[ $response == *"$SUCCESS_MESSAGE"* ]]; then
-        echo "Health check successful for server $1!"
-        exit 0
-    else
-        echo "Health check failed for server $1. Retrying in $DELAY_SECONDS seconds..."
-        sleep $DELAY_SECONDS
-        ((attempt++))
-    fi
-done
-
-echo "Maximum attempts reached. Health check failed for server $1."
-exit 1
-
+# 실행 확인
+echo "Deployment successful for server $1!"
+exit 0
