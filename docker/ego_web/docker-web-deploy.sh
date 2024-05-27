df715e5ea54fb630eb54b4c0642d7e02a2edf003
dentalseed <155122919+dentalseed@users.noreply.github.com>
2024-05-27 14:49:13 +0900

    Update docker-web-deploy.sh
    123

--- docker/ego_web/docker-web-deploy.sh
+++ docker/ego_web/docker-web-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <server_address>"
     exit 1
@@ -9,12 +8,11 @@ URL="$1:8080/health"
 SUCCESS_MESSAGE="SUCCESS"
 MAX_ATTEMPTS=40
 DELAY_SECONDS=1
-
-# 도커 컨테이너 중지 및 삭제
+# 도커 스탑, 삭제
 docker stop $1
 docker rm $1
 
-# API 배포
+# api 배포
 docker compose -p $1 -f  ./dev/$1-compose.yml build && docker compose -p $1 -f  ./dev/$1-compose.yml up -d
 
 # 실행 확인
@@ -22,7 +20,8 @@ attempt=1
 while [ $attempt -le $MAX_ATTEMPTS ]
 do
     echo "Attempting health check for server $1, attempt: $attempt"
-    response=$(docker exec $1 curl -sS "$URL")
+   # Nginx 컨테이너에서 요청... 8080포트로 접근하기위해
+    response=$( docker exec nginx curl -sS "$URL")
     if [[ $response == *"$SUCCESS_MESSAGE"* ]]; then
         echo "Health check successful for server $1!"
         exit 0
@@ -32,6 +31,5 @@ do
         ((attempt++))
     fi
 done
-
 echo "Maximum attempts reached. Health check failed for server $1."
 exit 1
